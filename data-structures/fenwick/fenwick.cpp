626a936adec15ba77727a6b266bd271bda499ae5
Nikolay Budin <n.budin@yandex.ru>
2022-10-30 11:21:29 +0800

    added another fenwick

--- data-structures/fenwick/fenwick.cpp
+++ data-structures/fenwick/fenwick.cpp
@@ -1,16 +1,22 @@
-const int MAXN = 1001;
-vector<ll> arr(MAXN);
-
-// adds $val$ at position $x$
-auto fenwick_add = [&](int x, ll val) {
-	for (; x < MAXN; x = x | (x + 1))
-		arr[x] += val;
-};
-
-// returns $\sum\limits_{i = 0}^{x} arr[i]$ ($x$ is inclusive)
-auto fenwick_get = [&](int x) {
-	ll res = 0;
-	for (; x >= 0; x = (x & (x + 1)) - 1)
-		res += arr[x];
-	return res;
+struct FT {
+	vector<ll> s;
+	FT(int n) : s(n) {}
+	void update(int pos, ll dif) { // a[pos] += dif
+		for (; pos < sz(s); pos |= pos + 1) s[pos] += dif;
+	}
+	ll query(int pos) { // sum of values in [0, pos)
+		ll res = 0;
+		for (; pos > 0; pos &= pos - 1) res += s[pos-1];
+		return res;
+	}
+	int lower_bound(ll sum) {// min pos st sum of [0, pos] >= sum
+		// Returns n if no sum is >= sum, or -1 if empty sum is.
+		if (sum <= 0) return -1;
+		int pos = 0;
+		for (int pw = 1 << 25; pw; pw >>= 1) {
+			if (pos + pw <= sz(s) && s[pos + pw-1] < sum)
+				pos += pw, sum -= s[pos-1];
+		}
+		return pos;
+	}
 };
